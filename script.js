712dc13c6ccff4d1154f4965bab43d60f0bafb61
JulianJua <juarezjulian078@gmail.com>
2025-08-19 21:16:33 -0700

    cm8

--- script.js
+++ script.js
@@ -213,7 +213,7 @@ document.addEventListener('DOMContentLoaded', function() {
         await new Promise(resolve => setTimeout(resolve, 500));
 
         heroTitle.style.opacity = '1';
-        await typeWriter(heroTitle, "Hello! I'm Julian!");
+        await typeWriter(heroTitle, "Hello, I'm Julian!");
         
         heroSubtitle.style.opacity = '1';
         heroSubtitle.style.transform = 'translateY(0)';
